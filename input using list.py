b5a6140254aa81e4f2ba530f07a760bf23ac2d34
Yashkumar-Shinde <44223914+Yashkumar-Shinde@users.noreply.github.com>
2018-10-24 08:33:02 +0530

    Add files via upload

---
+++ input using list.py
@@ -0,0 +1,19 @@
+x=input("enter your name:-")
+y=input("enter your gr no:-")
+z=input("enter your roll no:-")
+a=input("enter your marks in physics")
+b=input("enter your marks in maths")
+c=input("enter your marks in chemistry")
+
+x=[]
+x.append(input("enter your name:-"))
+y=[]
+y.append(input("enter your gr no:-"))
+z=[]
+z.append(input("enter your roll no:-"))
+a=[]
+a.append(input("enter your marks in physics"))
+b=[]
+b.append(input("enter your marks in maths"))
+c=[]
+c.append(input("enter your marks in chemistry"))
